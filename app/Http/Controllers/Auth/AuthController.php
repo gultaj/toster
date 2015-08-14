6d5302d686c1fa52f3c9b6c49a589a5dc2da594c
gultaj <yaschick.mail@gmail.com>
2015-08-14 15:05:24 +0300

    refactor

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,48 +2,44 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
-use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
-use App\Http\Requests;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Registration & Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users, as well as the
+	| authentication of existing users. By default, this controller uses
+	| a simple trait to add these behaviors. Why don't you explore it?
+	|
+	*/
 
-    use AuthenticatesAndRegistersUsers;
+	use AuthenticatesAndRegistersUsers;
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest', ['except' => 'getLogout']);
-    }
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest', ['except' => 'getLogout']);
+	}
 
-    public function getLogin()
-    {
-        return view('auth.login');
-    }
+	public function getLogin()
+	{
+		return view('auth.login');
+	}
 
-    public function postLogin(\App\Http\Requests\LoginUserRequest $request)
-    {
+	public function postLogin(\App\Http\Requests\LoginUserRequest $request)
+	{
+		if (\Auth::attempt($request->except('_token'), true))
+			return redirect()->intendent('home');
 
-        if (\Auth::attempt($request->except('_token'), true))
-            return redirect()->intendent('home');
-
-        return redirect()->back()->withInput();
-    }
+		return redirect()->back()->withInput();
+	}
 }
